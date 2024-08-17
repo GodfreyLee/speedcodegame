c36702d3ef42da6156b7abbb6749c005fac50658
GodfreyLee <godfreylee2000@gmail.com>
2024-08-17 20:26:02 +1000

    css-update

--- frontend/app/room/[roomId]/page.tsx
+++ frontend/app/room/[roomId]/page.tsx
@@ -1,17 +1,24 @@
 "use client";
 import Image from "next/image";
 import Embed from "react-runkit";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Editor from "@monaco-editor/react";
 import { cn } from "@/utils/tailwind";
 import Countdown from "react-countdown";
+import { answerQuestion, fetchRoomQuestion } from "@/actions/game";
 
-export default function Home() {
+export default function Home({ params }) {
+  let { roomId } = params;
+  roomId = parseInt(roomId);
+
   const [inputUsername, setInputUsername] = useState<any>();
   const [username, setUsername] = useState("");
   const [question, setQuestion] = useState(
     "Print all even numbers from 0 – 10"
   );
+  const [allPlayers, setAllPlayers] = useState([]);
+  const [gameQs, setGameQs] = useState([]);
+  const [questionNum, setQuestionNum] = useState(-1);
   const [consoleOutput, setConsoleOutput] = useState("");
   const [step, setStep] = useState("enterName");
   const [isCorrect, setIsCorrect] = useState(false);
@@ -31,6 +38,37 @@ export default function Home() {
     setStep("question");
   };
 
+  // fetch all question after enter room
+  // todo move this into waiting room
+  useEffect(() => {
+    const fetchQ = async () => {
+      const game = await fetchRoomQuestion(roomId);
+      console.log("🚀 ~ fetchQ ~ game.GameQuestions:", game);
+      setGameQs(game.GameQuestions);
+      setAllPlayers(game.Players);
+      setQuestionNum(0);
+    };
+    if (step === "question") {
+      fetchQ();
+    }
+  }, [step]);
+
+  // post answer to backend
+  useEffect(() => {
+    const answerQ = async (input) => {
+      const game = await answerQuestion(input);
+      // setGameQs(game.GameQuestions)
+      // setQuestionNum(0)
+    };
+    if (step === "score") {
+      answerQ({
+        player_id: allPlayers.find((v) => v.name === username).id,
+        game_question_id: gameQs[questionNum]?.QuestionBank?.id,
+        answer_text: consoleOutput,
+      });
+    }
+  }, [step]);
+
   function handleEditorDidMount(editor, monaco) {
     editorRef.current = editor;
   }
@@ -43,14 +81,15 @@ export default function Home() {
   const codeTemplate = `function solution() {` + `\n  return 1` + `\n}`;
   return (
     <div className="bg-[url('/bg-2.png')] h-[100vh] bg-cover">
-      <div className="bg-[#002265] p-5 flex justify-between">
+      <div className="bg-[#002265] p-2 flex justify-between">
         <div className="text-white">SpeedCodeGame</div>
+        <div className="text-white">Game Room {roomId}</div>
         {username && <div className="text-white">welcome, {username}</div>}
       </div>
       {step === "enterName" && (
         <div className="flex flex-col items-center gap-12 mt-12">
           <input
-            className="border p-2 rounded-md"
+            className="border p-2"
             placeholder="Enter your name"
             onKeyDown={(e) => {
               if (e.code === "Enter" || e.code === "NumpadEnter") {
@@ -62,7 +101,7 @@ export default function Home() {
             }}
           />
           <button
-            className="p-2 bg-[#1368CE] text-white rounded-md px-10"
+            className="p-2 bg-[#1368CE] text-white"
             onClick={() => {
               enterQuestionStage();
             }}
@@ -73,17 +112,13 @@ export default function Home() {
       )}
       {step === "question" && (
         <div className="flex flex-col items-center">
-          <div className="bg-white w-full font-bold p-5">
-            Questions: {question}
+          <div className="bg-white w-full p-2">
+            questions: {gameQs[questionNum]?.QuestionBank?.question_text || ""}
           </div>
-          <div className="flex mt-8">
+          <div>
             <Countdown
               renderer={({ hours, minutes, seconds, completed }) => {
-                return (
-                  <span className="bg-white p-3 rounded-full ml-auto">
-                    {minutes * 60 + seconds}
-                  </span>
-                );
+                return <span>{minutes * 60 + seconds}</span>;
               }}
               date={Date.now() + 60000}
             />
@@ -142,14 +177,14 @@ export default function Home() {
       {step === "score" && (
         <div className={cn("flex flex-col items-center")}>
           <div
-            className={cn("bg-white w-full p-5 flex font-bold justify-center", {
+            className={cn("bg-white w-full p-2 flex justify-center", {
               "text-red-500": !isCorrect,
               "text-green-500": isCorrect,
             })}
           >
             Your answer is {isCorrect ? "correct" : "wrong"}
           </div>
-          <div className="bg-[#002265] min-w-[300px] min-h-[400px] p-5 mt-12 text-white flex flex-col gap-2 rounded">
+          <div className="bg-[#002265] min-w-[300px] min-h-[400px] p-2 mt-12 text-white flex flex-col gap-2">
             {scoreList.map((s) => (
               <div className="flex justify-between">
                 <div>{s.username}</div>

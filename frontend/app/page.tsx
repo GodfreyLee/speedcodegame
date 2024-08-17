a6131a9dd2aff4a9c9b08a911fd1529945734845
lmfjeff <lmfjeff@gmail.com>
2024-08-17 15:13:26 +1000

    add score page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Embed from "react-runkit";
 import { useRef, useState } from "react";
 import Editor from "@monaco-editor/react";
+import { cn } from "@/utils/tailwind";
 
 export default function Home() {
   const [inputUsername, setInputUsername] = useState<any>();
@@ -10,7 +11,15 @@ export default function Home() {
   const [question, setQuestion] = useState(
     "Print all even numbers from 0 – 10"
   );
+  const [consoleOutput, setConsoleOutput] = useState("");
   const [step, setStep] = useState("enterName");
+  const [isCorrect, setIsCorrect] = useState(false);
+  const [scoreList, setScoreList] = useState([
+    { username: "Robyn", score: 982 },
+    { username: "Nancy", score: 911 },
+    { username: "Mal", score: 0 },
+    { username: "jeff", score: 0 },
+  ]);
   const editorRef = useRef(null);
   const enterQuestionStage = () => {
     if (!inputUsername) {
@@ -26,19 +35,21 @@ export default function Home() {
   }
   function showValue() {
     var expression = editorRef.current.getValue();
-    var a = eval('answer()');
-    console.log(a);
+    let result = eval(`(${expression})()`);
+    setConsoleOutput(result);
+    // alert(`result: ${result}`);
   }
+  const codeTemplate = `function solution() {` + `\n  return 1` + `\n}`;
   return (
-    <div className="bg-[url('/bg.png')] h-[100vh] bg-cover">
-      <div className="bg-blue-300 p-2 flex justify-between">
-        <div>SpeedCodeGame</div>
+    <div className="bg-[url('/bg-2.png')] h-[100vh] bg-cover">
+      <div className="bg-[#002265] p-2 flex justify-between">
+        <div className="text-white">SpeedCodeGame</div>
         {username && <div>welcome, {username}</div>}
       </div>
       {step === "enterName" && (
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center gap-12 mt-12">
           <input
-            className="border"
+            className="border p-2"
             placeholder="Enter your name"
             onKeyDown={(e) => {
               if (e.code === "Enter" || e.code === "NumpadEnter") {
@@ -50,7 +61,7 @@ export default function Home() {
             }}
           />
           <button
-            className="border"
+            className="p-2 bg-[#1368CE] text-white"
             onClick={() => {
               enterQuestionStage();
             }}
@@ -62,7 +73,7 @@ export default function Home() {
       {step === "question" && (
         <div className="flex flex-col items-center">
           <div className="bg-white w-full p-2">questions: {question}</div>
-          <div className="border max-w-[500px] w-full">
+          <div className="border max-w-[500px] w-full mt-12">
             {/* <Embed
               onEvaluate={(e: any) => {
                 console.log(e);
@@ -84,10 +95,11 @@ export default function Home() {
                 <Editor
                   height="50vh"
                   defaultLanguage="javascript"
-                  defaultValue='console.log("yes");'
+                  defaultValue={codeTemplate}
                   onMount={handleEditorDidMount}
                 />
               </div>
+              <div className="bg-white">{consoleOutput}</div>
               <div className="flex justify-between pt-2">
                 <div className="flex items-center space-x-5"></div>
                 <div className="flex-shrink-0">
@@ -100,6 +112,35 @@ export default function Home() {
                 </div>
               </div>
             </form>
+          </div>
+          <button
+            onClick={() => {
+              setStep("score");
+            }}
+            className="mt-4 inline-flex items-center rounded-md bg-[#26890C] px-3 py-2 text-sm font-semibold text-white hover:bg-green-500"
+          >
+            Submit
+          </button>
+        </div>
+      )}
+
+      {step === "score" && (
+        <div className={cn("flex flex-col items-center")}>
+          <div
+            className={cn("bg-white w-full p-2 flex justify-center", {
+              "text-red-500": !isCorrect,
+              "text-green-500": isCorrect,
+            })}
+          >
+            Your answer is {isCorrect ? "correct" : "wrong"}
+          </div>
+          <div className="bg-[#002265] min-w-[300px] min-h-[400px] p-2 mt-12 text-white flex flex-col gap-2">
+            {scoreList.map((s) => (
+              <div className="flex justify-between">
+                <div>{s.username}</div>
+                <div>{s.score}</div>
+              </div>
+            ))}
           </div>
         </div>
       )}
